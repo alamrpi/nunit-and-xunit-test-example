bfc95c6b3a40277a4e05e04f4e7a5a14b095bdad
Md Alam Hossain <alam.rpi@hotmail.com>
2023-02-23 17:58:39 +0600

    N unit test done

--- Sparky.NunitTest/BankAccountNUnitTests.cs
+++ Sparky.NunitTest/BankAccountNUnitTests.cs
@@ -57,5 +57,82 @@ namespace Sparky.NunitTest
             var result = bankAccount.Withdraw(withdraw);
             Assert.True(result);
         }
+
+        [Test]
+        [TestCase(200, 300)]
+        public void BankWithdraw_Withdraw300With200Balance_ReturnsFalse(int balance, int withdraw)
+        {
+            var logMoq = new Mock<ILogBook>();
+            logMoq.Setup(u => u.LogBalanceAfterWithdraw(It.Is<int>(x => x > 0))).Returns(true);
+            //logMoq.Setup(u => u.LogBalanceAfterWithdraw(It.Is<int>(x => x < 0))).Returns(false);
+            logMoq.Setup(u => u.LogBalanceAfterWithdraw(It.IsInRange(int.MinValue, -1, Moq.Range.Inclusive))).Returns(false);
+
+            BankAccount bankAccount = new BankAccount(logMoq.Object);
+            bankAccount.Deposite(balance);
+            var result = bankAccount.Withdraw(withdraw);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void BankLogDummy_LogMockString_ReturnTrue()
+        {
+            var logMoq = new Mock<ILogBook>();
+            string desiredOutput = "hello";
+
+            logMoq.Setup(u => u.MessageWithReturnStr(It.IsAny<string>())).Returns((string str) => str.ToLower());
+           
+            Assert.That(logMoq.Object.MessageWithReturnStr("Hello"), Is.EqualTo(desiredOutput));
+        }
+
+        [Test]
+        public void BankLogDummy_LogMockStringOutPutStr_ReturnTrue()
+        {
+            var logMoq = new Mock<ILogBook>();
+            string desiredOutput = "hello";
+
+            logMoq.Setup(u => u.LogWithOutputResult(It.IsAny<string>(), out desiredOutput)).Returns(true);
+            string result = "";
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(logMoq.Object.LogWithOutputResult("Ben", out result));
+                Assert.That(result, Is.EqualTo(desiredOutput));
+            });
+        }
+
+        [Test]
+        public void BankLogDummy_LogRefChecker_ReturnTrue()
+        {
+            var logMoq = new Mock<ILogBook>();
+            
+            Customer customer = new();
+            Customer customerNotUsed = new();
+
+            logMoq.Setup(u => u.LogWithRefObject(ref customer)).Returns(true);
+
+            Assert.IsFalse(logMoq.Object.LogWithRefObject(ref customerNotUsed));
+            Assert.IsTrue(logMoq.Object.LogWithRefObject(ref customer));
+        }
+
+        [Test]
+        public void BankLogDummy_SetAndGetLogTypeAndSeveirtyMock_MockTest()
+        {
+            var logMoq = new Mock<ILogBook>();
+         
+            logMoq.Setup(u => u.LogSeverity).Returns(10);
+            logMoq.Setup(u => u.LogType).Returns("warning");
+
+            //For setup all properties
+            //logMoq.SetupAllProperties();
+
+            //for setup single property
+            logMoq.SetupProperty(x => x.LogSeverity);
+            logMoq.Object.LogSeverity = 100;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(logMoq.Object.LogSeverity, Is.EqualTo(100));
+                Assert.That(logMoq.Object.LogType, Is.EqualTo("warning"));
+            });
+        }
     }
 }
--- Sparky/LogBook.cs
+++ Sparky/LogBook.cs
@@ -8,12 +8,22 @@ namespace Sparky
 {
     public interface ILogBook
     {
+        int LogSeverity { get; set; }
+        string LogType { get; set; }
+
         void Message(string message);
         bool LogToDb(string message);
         bool LogBalanceAfterWithdraw(int balanceAfterWithdraw);
+        string MessageWithReturnStr(string message);
+        bool LogWithOutputResult(string message, out string outputStr);
+
+        bool LogWithRefObject(ref Customer customer);
     }
     public class LogBook : ILogBook
     {
+        public int LogSeverity { get; set; }
+        public string LogType { get; set; }
+
         public bool LogBalanceAfterWithdraw(int balanceAfterWithdraw)
         {
             if(balanceAfterWithdraw >= 0)
@@ -32,9 +42,28 @@ namespace Sparky
             return true;
         }
 
+        public bool LogWithOutputResult(string message, out string outputStr)
+        {
+            outputStr = "Hello " + message;
+            return true;
+        }
+
+        public bool LogWithRefObject(ref Customer customer)
+        {
+            return true;
+        }
+
         public void Message(string message)
         {
            Console.WriteLine(message);
         }
+
+        public string MessageWithReturnStr(string message)
+        {
+            Console.WriteLine(message);
+            return message.ToLower();
+        }
+
+
     }
 }
